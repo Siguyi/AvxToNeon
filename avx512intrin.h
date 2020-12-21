7882250b4eb989cea6716244704b941f685ff369
lipingEmma <15396232681@163.com>
2020-12-21 13:05:04 +0800

    change my head file

--- avx512intrin.h
+++ avx512intrin.h
@@ -120,14 +120,14 @@ FORCE_INLINE __m512i _mm512_div_epu32(__m512i a, __m512i b)
 {
     __m512i res_m512i;
     res_m512i.vect_i256[0] = _mm256_div_epu32(a.vect_i256[0], b.vect_i256[0]);
-    //res_m512i.vect_i256[1] = _mm256_div_epu32(a.vect_i256[1], b.vect_i256[1]);
+    res_m512i.vect_i256[1] = _mm256_div_epu32(a.vect_i256[1], b.vect_i256[1]);
     return res_m512i;
 }
 
 FORCE_INLINE __m512i _mm512_div_epu64(__m512i a, __m512i b)
 {
     __m512i res_m512i;
-    res_m512i.vect_i256[0] = _mm256_div_epu64(a.vect_i256[0], b.vect_i256[0]);
+    //res_m512i.vect_i256[0] = _mm256_div_epu64(a.vect_i256[0], b.vect_i256[0]);
     res_m512i.vect_i256[1] = _mm256_div_epu64(a.vect_i256[1], b.vect_i256[1]);
     return res_m512i;
 }
